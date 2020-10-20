7ded1fc594bf692c812faeda9c9e1bb98ad13b6d
Luís Fernando de Lucca Costa Souza <luisfernandodelucca@hotmail.com>
2020-10-20 00:44:24 -0300

    Delete footer.php

--- views/footer.php
+++
@@ -1,6 +0,0 @@
-        <footer class="footer-copyright text-center mt-4 py-4 bg-info text-white">
-            © 2020 Copyright: Luís Fernando de L. C. Souza <br />
-            Made with HTML, Bootstrap and PHP
-        </footer>
-    </body>
-</html>
